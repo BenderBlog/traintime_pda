c900919ea3bc8a6f6255899324e4d5010fbf10e6
David "SuperBart" Chen <Songze@live.cn>
2023-07-23 21:47:28 +0800

    refactor: score info card tidy

--- lib/page/score/score_info.dart
+++ lib/page/score/score_info.dart
@@ -36,186 +36,193 @@ class _ScoreComposeCardState extends State<ScoreComposeCard> {
         elevation: 0,
         color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
         child: GetBuilder<ScoreController>(
-          builder: (c) => FutureBuilder<ScorePlace>(
-            future: isInClass ? inClassPlace : inGradePlace,
-            builder: (context, snapshot) {
-              List<Widget> info = [];
-              if (snapshot.hasData) {
-                if (snapshot.data!.highest != null) {
-                  info.add(InfoDetailBox(
-                    child: Table(
-                      children: [
-                        if (snapshot.data!.place != null)
-                          TableRow(
-                            children: <Widget>[
-                              const TableCell(
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text("排名"),
-                                ),
-                              ),
-                              TableCell(
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text(
-                                      "${snapshot.data!.place} / ${snapshot.data!.total}"),
-                                ),
-                              ),
-                            ],
+          builder: (c) {
+            Widget table(ScorePlace c) => Table(
+                  children: [
+                    if (c.place != null)
+                      TableRow(
+                        children: <Widget>[
+                          const TableCell(
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text("排名"),
+                            ),
                           ),
-                        TableRow(
-                          children: <Widget>[
-                            const TableCell(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text("最高分"),
-                              ),
+                          TableCell(
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Text("${c.place} / ${c.total}"),
                             ),
-                            TableCell(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(snapshot.data!.highest.toString()),
-                              ),
-                            ),
-                          ],
+                          ),
+                        ],
+                      ),
+                    TableRow(
+                      children: <Widget>[
+                        const TableCell(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("最高分"),
+                          ),
                         ),
-                        TableRow(
-                          children: <Widget>[
-                            const TableCell(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text("最低分"),
-                              ),
-                            ),
-                            TableCell(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(snapshot.data!.lowest.toString()),
-                              ),
-                            ),
-                          ],
-                        ),
-                        TableRow(
-                          children: <Widget>[
-                            const TableCell(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text("平均分"),
-                              ),
-                            ),
-                            TableCell(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(snapshot.data!.average.toString()),
-                              ),
-                            ),
-                          ],
+                        TableCell(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(c.highest.toString()),
+                          ),
                         ),
                       ],
                     ),
-                  ));
-                }
-                if (snapshot.data!.statistics.isNotEmpty) {
-                  info.add(const SizedBox(height: 10));
-                  info.add(
-                    SizedBox(
-                      height: 176,
-                      child: InfoDetailBox(
-                        child: Chart(
-                          data: [
-                            for (var i in snapshot.data!.statistics)
-                              {'level': i.level, 'people': i.people},
-                          ],
-                          variables: {
-                            'level': Variable(
-                              accessor: (Map map) => map['level'] as String,
-                            ),
-                            'people': Variable(
-                              accessor: (Map map) => map['people'] as int,
-                            ),
-                          },
-                          marks: [
-                            IntervalMark(
-                              label: LabelEncode(
-                                  encoder: (tuple) =>
-                                      Label(tuple['people'].toString())),
-                              color: ColorEncode(
-                                value: Defaults.primaryColor,
-                                updaters: {
-                                  'tap': {
-                                    false: (color) => color.withAlpha(100)
-                                  }
-                                },
+                    TableRow(
+                      children: <Widget>[
+                        const TableCell(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("最低分"),
+                          ),
+                        ),
+                        TableCell(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(c.lowest.toString()),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: <Widget>[
+                        const TableCell(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("平均分"),
+                          ),
+                        ),
+                        TableCell(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(c.average.toString()),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+            return FutureBuilder<ScorePlace>(
+              future: isInClass ? inClassPlace : inGradePlace,
+              builder: (context, snapshot) {
+                List<Widget> info = [];
+                if (snapshot.hasData) {
+                  if (snapshot.data!.highest != null) {
+                    info.add(InfoDetailBox(child: table(snapshot.data!)));
+                  }
+                  if (snapshot.data!.statistics.isNotEmpty) {
+                    info.add(const SizedBox(height: 10));
+                    info.add(
+                      SizedBox(
+                        height: 176,
+                        child: InfoDetailBox(
+                          child: Chart(
+                            data: [
+                              for (var i in snapshot.data!.statistics)
+                                {'level': i.level, 'people': i.people},
+                            ],
+                            variables: {
+                              'level': Variable(
+                                accessor: (Map map) => map['level'] as String,
                               ),
-                            ),
-                          ],
-                          axes: [
-                            Defaults.horizontalAxis,
-                            Defaults.verticalAxis,
-                          ],
+                              'people': Variable(
+                                accessor: (Map map) => map['people'] as int,
+                              ),
+                            },
+                            marks: [
+                              IntervalMark(
+                                label: LabelEncode(
+                                  encoder: (tuple) => Label(
+                                    tuple['people'].toString(),
+                                  ),
+                                ),
+                                color: ColorEncode(
+                                  value: Defaults.primaryColor,
+                                  updaters: {
+                                    'tap': {
+                                      false: (color) => color.withAlpha(100)
+                                    }
+                                  },
+                                ),
+                              ),
+                            ],
+                            axes: [
+                              Defaults.horizontalAxis,
+                              Defaults.verticalAxis,
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                }
-                if (info.isEmpty) {
+                    );
+                  }
+                  if (info.isEmpty) {
+                    info.add(
+                      InfoDetailBox(
+                        child: Center(
+                          child: Text("目前没有${isInClass ? "班级" : "年级"}成绩详情信息"),
+                        ),
+                      ),
+                    );
+                  }
+                } else if (snapshot.hasError) {
                   info.add(
                     InfoDetailBox(
                       child: Center(
-                        child: Text("目前没有${isInClass ? "班级" : "年级"}成绩详情信息"),
+                        child: Text("未获取${isInClass ? "班级" : "年级"}成绩详情信息"),
+                      ),
+                    ),
+                  );
+                } else {
+                  info.add(
+                    const InfoDetailBox(
+                      child: Center(
+                        child: Text("正在获取"),
                       ),
                     ),
                   );
                 }
-              } else if (snapshot.hasError) {
-                info.add(
-                  InfoDetailBox(
-                    child: Center(
-                      child: Text("未获取${isInClass ? "班级" : "年级"}成绩详情信息"),
-                    ),
+                return Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${isInClass ? "班级" : "年级"}信息",
+                        textScaleFactor: 1.1,
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      ...info
+                    ],
                   ),
                 );
-              } else {
-                info.add(
-                    const InfoDetailBox(child: Center(child: Text("正在获取"))));
-              }
-              return Container(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${isInClass ? "班级" : "年级"}信息",
-                      textScaleFactor: 1.1,
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    ...info
-                  ],
-                ),
-              );
-            },
-          ),
+              },
+            );
+          },
         ),
       );
 
+  var watermark = Watermark(
+    rowCount: 3,
+    columnCount: 8,
+    text: "${preference.getString(preference.Preference.idsAccount)} "
+        "${preference.getString(preference.Preference.name)} \n"
+        "仅个人参考 他用无效 ",
+    textStyle: const TextStyle(
+      color: Color(0x48000000),
+      fontSize: 12,
+      height: 4,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    var watermark = Watermark(
-      rowCount: 3,
-      columnCount: 8,
-      text: "${preference.getString(preference.Preference.idsAccount)} "
-          "${preference.getString(preference.Preference.name)} \n"
-          "仅个人参考 他用无效 ",
-      textStyle: const TextStyle(
-        color: Color(0x48000000),
-        fontSize: 12,
-        height: 4,
-      ),
-    );
-
     return Stack(
       children: [
         ListView(
@@ -229,36 +236,41 @@ class _ScoreComposeCardState extends State<ScoreComposeCard> {
                   future: compose,
                   builder: (context, snapshot) {
                     late Widget info;
+
                     if (snapshot.hasData) {
                       if (snapshot.data == null ||
                           snapshot.data!.score.isEmpty) {
                         info = const InfoDetailBox(
                             child: Center(child: Text("未提供详情信息")));
                       } else {
+                        TableRow scoreDetail(ComposeDetail i) {
+                          return TableRow(
+                            children: <Widget>[
+                              TableCell(
+                                child: Text(i.content),
+                              ),
+                              TableCell(
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(i.ratio),
+                                ),
+                              ),
+                              TableCell(
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(i.score),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+
                         info = InfoDetailBox(
                           child: Table(
-                            children: [
-                              for (var i in snapshot.data!.score)
-                                TableRow(
-                                  children: <Widget>[
-                                    TableCell(
-                                      child: Text(i.content),
-                                    ),
-                                    TableCell(
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Text(i.ratio),
-                                      ),
-                                    ),
-                                    TableCell(
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Text(i.score),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                            ],
+                            children: List<TableRow>.generate(
+                              snapshot.data!.score.length,
+                              (i) => scoreDetail(snapshot.data!.score[i]),
+                            ),
                           ),
                         );
                       }
