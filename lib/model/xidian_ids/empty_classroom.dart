24682b853b24aea2fe2e20bafe3485f7c51414f5
David "SuperBart" Chen <Songze@live.cn>
2023-08-07 15:21:32 +0800

    feat: empty classroom api

---
+++ lib/model/xidian_ids/empty_classroom.dart
@@ -0,0 +1,27 @@
+class EmptyClassroomPlace {
+  final String name;
+  final String code;
+
+  EmptyClassroomPlace({
+    required this.name,
+    required this.code,
+  });
+}
+
+class EmptyClassroomData {
+  final String name;
+  final bool isEmpty1To2;
+  final bool isEmpty3To4;
+  final bool isEmpty5To6;
+  final bool isEmpty7To8;
+  final bool isEmpty9To10;
+
+  EmptyClassroomData({
+    required this.name,
+    required this.isEmpty1To2,
+    required this.isEmpty3To4,
+    required this.isEmpty5To6,
+    required this.isEmpty7To8,
+    required this.isEmpty9To10,
+  });
+}
