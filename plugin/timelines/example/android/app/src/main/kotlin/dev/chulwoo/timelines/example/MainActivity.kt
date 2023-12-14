203d0bea67b1fa6c599e79e0a73a9b581e3bee4f
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2023-12-14 22:21:39 +0800

    Revert "chore: fork timelines package"
    This reverts commit 49a8e59f6eaae154e780be7d2e392ea87563f3b3.

--- plugin/timelines/example/android/app/src/main/kotlin/dev/chulwoo/timelines/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.chulwoo.timelines.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- plugin/timelines/lib/src/util.dart
+++
@@ -1,1 +0,0 @@
-const kFlexMultiplier = 1000.0;
