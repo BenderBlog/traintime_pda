17ca9354f045ac8d25ebd8208506a0f0101f989d
David "SuperBart" Chen <Songze@live.cn>
2024-07-02 14:41:18 +0800

    fix: message fetch fix

--- lib/repository/message_session.dart
+++ lib/repository/message_session.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+import 'package:synchronized/synchronized.dart';
 import 'package:watermeter/repository/network_session.dart';
 import 'package:watermeter/model/message/message.dart';
 
@@ -15,35 +16,30 @@ Dio get dio => Dio()..interceptors.add(aliceDioAdapter);
 
 const url = "https://legacy.superbart.top/traintime_pda_backend";
 
-// Never used lol...
-Future<UpdateMessage> checkUpdate() async {
-  return await dio.get("$url/version.json").then(
-        (value) => UpdateMessage.fromJson(value.data),
-      );
-}
+final messageLock = Lock(reentrant: false);
 
-Future<void> checkMessage() async {
-  var file = File("${supportPath.path}/Notice.json");
-  bool isExist = file.existsSync();
-  List<NoticeMessage> toAdd = [];
+Future<void> checkMessage() => messageLock.synchronized(() async {
+      var file = File("${supportPath.path}/Notice.json");
+      bool isExist = await file.exists();
+      List<NoticeMessage> toAdd = [];
 
-  try {
-    toAdd = await dio.get("$url/message.json").then(
-          (value) => List<NoticeMessage>.generate(
-            value.data.length,
-            (index) => NoticeMessage.fromJson(value.data[index]),
-          ),
-        );
-    file.writeAsStringSync(jsonEncode(toAdd));
-  } on Exception {
-    if (isExist) {
-      toAdd = jsonDecode(file.readAsStringSync());
-    } else {
-      toAdd = [];
-    }
-  }
+      try {
+        toAdd = await dio.get("$url/message.json").then(
+              (value) => List<NoticeMessage>.generate(
+                value.data.length,
+                (index) => NoticeMessage.fromJson(value.data[index]),
+              ),
+            );
+        file.writeAsStringSync(jsonEncode(toAdd));
+      } on Exception {
+        if (isExist) {
+          toAdd = jsonDecode(file.readAsStringSync());
+        } else {
+          toAdd = [];
+        }
+      }
 
-  messages.clear();
-  messages.addAll(toAdd);
-  // Add cache.
-}
+      messages.clear();
+      messages.addAll(toAdd);
+      // Add cache.
+    });
