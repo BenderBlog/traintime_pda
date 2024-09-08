a1319ca107bc20e82af8934e0475ade0020fb041
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2024-09-08 16:26:14 +0800

    feat: use disclosure for notification component

--- lib/page/homepage/info_widget/notice_card/notice_list.dart
+++ lib/page/homepage/info_widget/notice_card/notice_list.dart
@@ -1,9 +1,12 @@
 // Copyright 2023 BenderBlog Rodriguez and contributors.
 // SPDX-License-Identifier: MPL-2.0
 
+import 'package:disclosure/disclosure.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:styled_widget/styled_widget.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:watermeter/page/public_widget/both_side_sheet.dart';
 import 'package:watermeter/repository/message_session.dart';
 import 'package:watermeter/page/public_widget/public_widget.dart';
 
@@ -13,8 +16,63 @@ class NoticeList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => SimpleDialog(
-        title: const Text("应用信息"),
+      () => BothSideSheet(
+        title: "应用信息",
+        child: DisclosureGroup(
+          multiple: false,
+          clearable: true,
+          children: List<Widget>.generate(messages.length, (index) {
+            return Disclosure(
+              key: ValueKey('disclosure-$index'),
+              wrapper: (state, child) {
+                return Card.outlined(
+                  clipBehavior: Clip.antiAlias,
+                  child: child,
+                );
+              },
+              header: DisclosureButton(
+                child: ListTile(
+                  title: Row(
+                    children: [
+                      TagsBoxes(text: messages[index].type),
+                      const SizedBox(width: 8),
+                      Flexible(
+                        child: Text(
+                          messages[index].title,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: const DisclosureSwitcher(
+                    opened: Icon(Icons.arrow_drop_up),
+                    closed: Icon(Icons.arrow_drop_down),
+                  ),
+                ),
+              ),
+              divider: const Divider(height: 1),
+              child: Builder(builder: (context) {
+                if (bool.tryParse(messages[index].isLink) ?? false) {
+                  return FilledButton.icon(
+                    onPressed: () => launchUrlString(
+                      messages[index].message,
+                      mode: LaunchMode.externalApplication,
+                    ),
+                    label: const Text("访问该链接"),
+                    icon: const Icon(Icons.ads_click),
+                  ).center();
+                }
+                return SelectableText(messages[index].message);
+              }).padding(all: 12),
+            );
+          }),
+        ).scrollable().padding(top: 20),
+      ),
+    );
+
+    /*
+      SimpleDialog(
+        title: const Text(),
         children: List.generate(
           messages.length,
           (index) => SimpleDialogOption(
@@ -49,6 +107,6 @@ class NoticeList extends StatelessWidget {
           ),
         ),
       ),
-    );
+    );*/
   }
 }
