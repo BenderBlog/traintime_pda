68d8aff98b3cc2e760bd9543504254591bedd47a
BenderBlog Rodriguez <superbart_chen@qq.com>
2025-09-03 23:27:31 +0800

    feat: class card position always at bottom  - change some spdx notice

--- lib/page/classtable/arrangement_detail/arrangement_list.dart
+++ lib/page/classtable/arrangement_detail/arrangement_list.dart
@@ -1,6 +1,6 @@
 // Copyright 2023-2025 BenderBlog Rodriguez and contributors
 // Copyright 2025 Traintime PDA authors.
-// SPDX-License-Identifier: MPL-2.0 OR  Apache-2.0
+// SPDX-License-Identifier: MPL-2.0 OR Apache-2.0
 
 import 'package:flutter/material.dart';
 import 'package:watermeter/model/xidian_ids/classtable.dart';
