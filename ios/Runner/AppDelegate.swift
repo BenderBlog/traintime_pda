521157a169024cf8a57a1272f830ea2061176b37
叶月枫 <yeyuefeng699@qq.com>
2025-11-06 13:22:20 +0800

    chore: finish the notification module setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      // This is required to make any communication available in the action isolate.
+      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+          GeneratedPluginRegistrant.register(with: registry)
+      }
+
+      if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+      }
+
       GeneratedPluginRegistrant.register(with: self)
       
       let controller = window?.rootViewController as! FlutterViewController
@@ -14,5 +24,18 @@ import Flutter
       SaveToGroupIdSwiftApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: api)
       
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  @available(iOS 12.0, *)
+  override func userNotificationCenter(
+      _ center: UNUserNotificationCenter,
+      openSettingsFor notification: UNNotification?
+  ) {
+      let controller = window?.rootViewController as! FlutterViewController
+      let channel = FlutterMethodChannel(
+          name: "com.example.flutter_local_notifications_example/settings",
+          binaryMessenger: controller.binaryMessenger)
+
+      channel.invokeMethod("showNotificationSettings", arguments: nil)
   }
 }
