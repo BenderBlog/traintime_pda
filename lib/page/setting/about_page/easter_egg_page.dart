246ad9ca9a4ad593e0d17d5cd199612d797dca61
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2025-12-18 00:03:31 +0800

    chore: season specific easter egg

--- lib/page/setting/about_page/easter_egg_page.dart
+++ lib/page/setting/about_page/easter_egg_page.dart
@@ -2,10 +2,12 @@
 // Copyright 2025 Traintime PDA authors.
 // SPDX-License-Identifier: MPL-2.0
 
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:sakura_blizzard/sakura_blizzard.dart';
+//import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:styled_widget/styled_widget.dart';
-import 'package:watermeter/page/public_widget/toast.dart';
+//import 'package:watermeter/page/public_widget/toast.dart';
 
 class EasterEggPage extends StatefulWidget {
   const EasterEggPage({super.key});
@@ -15,15 +17,55 @@ class EasterEggPage extends StatefulWidget {
 }
 
 class _EasterEggPageState extends State<EasterEggPage> {
-  int counter = 0;
+  //int counter = 0;
+
+  bool isNotApple = (!Platform.isIOS && !Platform.isMacOS);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(FlutterI18n.translate(context, "easter_egg_robot.appbar")),
+        title: Text(
+          isNotApple ? "Merry Xmas" : "Early Sakura",
+        ), //Text(FlutterI18n.translate(context, "easter_egg_robot.appbar")),
       ),
-      body: [
+      body: LayoutBuilder(
+        builder: (context, constraits) {
+          if (isNotApple) {
+            return SnowFallView(
+              minBrightness: 0.2,
+              viewSize: Size(constraits.maxWidth, constraits.maxHeight),
+              fps: 60,
+              child: Align(
+                alignment: AlignmentGeometry.bottomCenter,
+                child: Image.asset("assets/art/anan_christmas.png"),
+              ),
+            ).decorated(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blue[900]!, Colors.blue[700]!],
+              ),
+            );
+          } else {
+            return SakuraBlizzardView(
+              viewSize: Size(constraits.maxWidth, constraits.maxHeight),
+              fps: 60,
+              frontObjNum: 30,
+              backObjNum: 0,
+              child: ConstrainedBox(
+                constraints: BoxConstraints.expand(),
+                child: Image.asset(
+                  "assets/art/Sakura_and_Tomoyo_Kimono.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          }
+        },
+      ),
+
+      /*[
         Image.asset("assets/art/aboutRobots-icon.png"),
         const SizedBox(height: 24),
         Text(
@@ -59,7 +101,7 @@ class _EasterEggPageState extends State<EasterEggPage> {
             ),
           ),
         ),
-      ].toColumn().scrollable().center().padding(horizontal: 16),
+      ].toColumn().scrollable().center().padding(horizontal: 16),*/
     );
   }
 }
