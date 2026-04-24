c6ec5bdacdc9ae12bd9ad564eedac435a83b19b1
SolitaryDream-X <xubinxun@outlook.com>
2026-04-24 15:57:20 +0800

    feat: 处理冲突

--- lib/repository/preference.dart
+++ lib/repository/preference.dart
@@ -90,6 +90,15 @@ enum Preference {
   dormWaterToken(key: "dorm_water_token", type: "String"), // 宿舍水机登录 token
   dormWaterUid(key: "dorm_water_uid", type: "String"), // 宿舍水机用户 uid
   dormWaterEid(key: "dorm_water_eid", type: "String"), // 宿舍水机用户 eid
+  systemCalendarId(key: "system_calendar_id", type: "String"), // 导出的系统日历 id
+  systemCalendarSemesterCode(
+    key: "system_calendar_semester_code",
+    type: "String",
+  ), // 导出的系统日历绑定学期
+  systemCalendarSnapshot(
+    key: "system_calendar_snapshot",
+    type: "String",
+  ), // 上次同步到系统日历的数据快照
   currentTimeIndicatorEnabled(
     key: "currentTimeIndicatorEnabled",
     type: "bool",
