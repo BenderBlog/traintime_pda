998103407daa8c264beb426389dfaf4be93f1d26
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2026-08-06 23:44:39 +0800

    feat: new setting page

--- lib/page/energy/energy_ready_view.dart
+++ lib/page/energy/energy_ready_view.dart
@@ -4,7 +4,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:styled_widget/styled_widget.dart';
-import 'package:watermeter/page/energy/aircon_energy_card.dart';
 import 'package:watermeter/page/energy/electricity_energy_card.dart';
 import 'package:watermeter/page/energy/water_energy_card.dart';
 import 'package:watermeter/page/public_widget/public_widget.dart';
